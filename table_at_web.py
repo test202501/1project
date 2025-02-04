843dcedb9ebc0730b79efb9d1098519da7b53dc5
test202501 <somebody4891@o2.pl>
2025-02-04 23:21:01 +0100

    change bootstrap layout table to tailwind CSS layout

--- table_at_web.py
+++ table_at_web.py
@@ -6,10 +6,10 @@ app = Flask(__name__)
 
 # read data from Excel file 
 
-data = pd.read_excel('C:/Praktyczny_Python/ROBOCZE/tabela kursów NBP z dnia 2024-05-06.xlsx')
+data = pd.read_excel('C:/Praktyczny_Python/ROBOCZE/tabela kursów NBP z dnia 2023-11-10.xlsx', na_filter = '', skiprows=1)
 
 # transform data into HTML
-html_table = data.to_html(classes='table table-striped', index=False)
+html_table = data.to_html(classes='min-w-full divide-y divide-gray-100 hover:bg-green-100 border bold border-green-300', index=False, col_space=10)
 
 @app.route('/')
 def home():
@@ -21,16 +21,18 @@ def home():
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Dane z Excel</title>
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css">
 
         <style>
             th {text-align: left;
             font-weight: bold;
-            background-color: #AEEEEE;}
+            font-underlined: true;
+            font-size: 13;
+            background-color: #DF0050;}
         </style>    
     </head>
     <body>
-        <div class="container">
+        <div class="container mx-auto p-4">
             <h1>Dane z Excel</h1>
             {{ table|safe }}
         </div>
